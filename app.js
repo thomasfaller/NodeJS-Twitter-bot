7ef7d2577517198812954d876a36857f08666f5e
Thomas Faller <thomasfaller@Thomass-MacBook-Pro.local>
2018-01-12 21:28:16 +0000

    added the loop to iterate through the response array

--- app.js
+++ app.js
@@ -13,14 +13,28 @@ var params = {
 
 T.get('search/tweets', params, function(err, data, response) {
     if (!err) {
-
-
-        // Here's what gets excuted if the GET request passes
-
-
+        
+        // Loop through the returned tweets
+    for(let i = 0; i < data.statuses.length; i++){
+        // Get the tweet Id from the returned data
+        let id = { id: data.statuses[i].id_str }
+        // Try to Favorite the selected Tweet
+        T.post('favorites/create', id, function(err, response){
+          // If the favorite fails, log the error message
+          if(err){
+            console.log(err[0].message);
+          }
+          // If the favorite is successful, log the url of the tweet
+          else{
+            let username = response.user.screen_name;
+            let tweetId = response.id_str;
+            console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+          }
+        });
+      }
 
 
     } else {
-        console.log('There was an error with the GET request. Please try again later...');
+        console.log(err);
     }
 });
--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    consumer_key: 'OiLcCutwbHo0uY8DTJoDINjNh',
-    consumer_secret: 'kHGYFZp3ZrzHhPDGvetracn9RvZ9aH6P5skRt0On3s0pGy2XCM',
+    consumer_key: 'p40UE6YFJIHdOHz7QWfiiw2B9',
+    consumer_secret: 'fGHteTV2gpXzueEpaq9HCbHwVbqB4bEHV533wr5nbqiGMLAPCn',
     access_token: '26268331-zSWGUIJsPKSE5ATWINgdHVpwlkzKFhAbmVMxk8sKJ',
-    access_token_secret: 'NdImUtOTSVa7p6mqNKtQtlXhr4kmsmSzWTvYPaJJexnR2'
+    access_token_secret: 'NdImUtOTSVa7p6mqNKtQtlXhr4kmsmSzWTvYPaJJexnR2',
+    app_only_auth: true
 };
 
