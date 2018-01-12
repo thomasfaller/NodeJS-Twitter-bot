6b6ccdb3f259527910f84c1696307ec89bd1fbd1
Thomas Faller <thomasfaller@Thomass-MacBook-Pro.local>
2018-01-12 22:15:11 +0000

    updated token keys

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    consumer_key: 'p40UE6YFJIHdOHz7QWfiiw2B9',
-    consumer_secret: 'fGHteTV2gpXzueEpaq9HCbHwVbqB4bEHV533wr5nbqiGMLAPCn',
-    access_token: '26268331-zSWGUIJsPKSE5ATWINgdHVpwlkzKFhAbmVMxk8sKJ',
-    access_token_secret: 'NdImUtOTSVa7p6mqNKtQtlXhr4kmsmSzWTvYPaJJexnR2',
-    app_only_auth: true
+    consumer_key: 'JoaAX8v79luVxykP1MOS5hk1H',
+    consumer_secret: 'oCzqvl5gFJobJWdqxnlGs30ktJsGMtNwrMXzYdEvTwk45NucXq',
+    access_token_key: '26268331-KtqpIG2G01AaNtZNA5ayqguibNLKzxGqYHOWshnSu',
+    access_token_secret: 'lw3KDqWAw0E51sFCJd3bFefAaafnFrl9WVBn1rL85LKDv'
 };
 
