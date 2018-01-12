3cdacba920660b2f13e2200beb07f25051711eaa
Thomas Faller <thomasfaller@Thomass-MacBook-Pro.local>
2018-01-12 21:09:28 +0000

    Created the GET request structure for the tweets

--- app.js
+++ app.js
@@ -4,5 +4,23 @@ var T = new Twitter(config);
 
 var params = {
     q: '#nodejs',
-    
+    count: 10,
+    result_type: 'recent',
+    lang: 'eng'
 }
+
+// The GET request
+
+T.get('search/tweets', params, function(err, data, response) {
+    if (!err) {
+
+
+        // Here's what gets excuted if the GET request passes
+
+
+
+
+    } else {
+        console.log('There was an error with the GET request. Please try again later...');
+    }
+});
